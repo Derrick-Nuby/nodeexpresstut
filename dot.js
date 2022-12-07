93c772ac68d8913173163fd455e77c820ba15857
Derrick-Nuby <iradukundaderrick7@gmail.com>
2022-12-07 22:00:22 +0200

    added all

--- dot.js
+++ dot.js
@@ -1,8 +1,19 @@
-const {readFileSync, writeFileSync} = require('fs');
+const http = require('http');
 
-const first = readFileSync('./content/first.txt', 'utf8');
-const second = readFileSync('./content/second.txt', 'utf8');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('hello Node JS');
+        
+    }
 
-// console.log(first, second);
+    if (req.url === '/about'){
+        res.end('hello is our about');
+    }
+    res.end(
+       ` <h1> Hello ERRORS</h1>
+        <p>we can't seem to find the page you are looking for</p>
+        <a href = "/">Bach home</a>`
+    )
+})
 
-writeFileSync('./content/check.txt', `here is what we have: ${first}, ${second}`)
+server.listen(5000);

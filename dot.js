2c6d8ad1abe8efd1727398b13bdb275ed2e342b7
Derrick-Nuby <iradukundaderrick7@gmail.com>
2022-12-07 12:09:56 +0200

    done path module

--- dot.js
+++ dot.js
@@ -1,4 +1,11 @@
 const path = require('path');
 
 
-console.log(path.join);
+// console.log(path.join);
+// console.log(path.sep);
+
+// const filepath = path.join('/1st-Tut', 'more', 'ing.txt');
+// console.log(filepath);
+
+const absolute = path.resolve(__dirname, '/1st-Tut', 'more', 'ing.txt');
+console.log(absolute);

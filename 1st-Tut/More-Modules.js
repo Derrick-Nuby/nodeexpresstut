12038c4d28552a0c63a09209fc1db3a53c20fc82
Derrick-Nuby <iradukundaderrick7@gmail.com>
2022-12-07 12:02:35 +0200

    day2

---
+++ 1st-Tut/More-Modules.js
@@ -0,0 +1,25 @@
+// const os = require('os');
+
+// const user = os.userInfo();
+
+// console.log(user);
+
+const os = require('os');
+
+// const user = os.userInfo();
+
+// const user = os.uptime();
+
+// const user = os.type();
+
+// const user = os.release();
+
+// const user = os.totalmem();
+
+const user = os.freemem();
+
+
+
+
+console.log(user);
+

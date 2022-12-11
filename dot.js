472c74e922a9b159ec0325feaab95fc4b0df3a10
Derrick-Nuby <iradukundaderrick7@gmail.com>
2022-12-11 14:44:49 +0200

    all

--- dot.js
+++ dot.js
@@ -8,12 +8,13 @@ const server = http.createServer((req, res) => {
 
     if (req.url === '/about'){
         res.end('hello is our about');
-    }
+    } else {
     res.end(
        ` <h1> Hello ERRORS</h1>
         <p>we can't seem to find the page you are looking for</p>
         <a href = "/">Bach home</a>`
-    )
+        
+    );}
 })
 
 server.listen(5000);
